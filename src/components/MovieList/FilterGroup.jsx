cb59b988de18847332a7142066e79b9a2a1996ef
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-10-25 19:52:04 +0200

    correction of filter group

--- src/components/MovieList/FilterGroup.jsx
+++ src/components/MovieList/FilterGroup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FilterGroup = ({minRatin, handleFilter, ratings}) => {
   return (
     <ul className="align_center movie_filter">
-        {ratings.map(rating=><li className={minRatin===rating ?"movie_filter_item active": "movie_filter_item"} key={rate} onClick={()=>{handleFilter(rating)}} >{rating}+ Star</li>)}
+        {ratings.map(rating=><li className={minRatin===rating ?"movie_filter_item active": "movie_filter_item"} key={rating} onClick={()=>{handleFilter(rating)}} >{rating}+ Star</li>)}
                 </ul>
   )
 }
